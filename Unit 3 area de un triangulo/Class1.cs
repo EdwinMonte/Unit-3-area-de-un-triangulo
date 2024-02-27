af99713075fe0628bcee175c2704a85338f685e3
drp_3 <drp_3@DESKTOP-T87EQCP>
2024-02-26 23:18:52 -0600

    Agregar archivos de proyecto.

---
+++ Unit 3 area de un triangulo/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Triangulo
+{
+    
+    private double baseT; 
+    private double altura; 
+    private double ladoA; 
+    private double ladoB; 
+    private double ladoC; 
+
+    public Triangulo(double baseT, double altura, double ladoA, double ladoB, double ladoC)
+    {
+        this.baseT = baseT;
+        this.altura = altura;
+        this.ladoA = ladoA;
+        this.ladoB = ladoB;
+        this.ladoC = ladoC;
+    }
+    
+    ~Triangulo() { }
+    
+    public double AreaBaseAltura()
+    {
+        return (baseT * altura) / 2;
+    }
+
+    public double AreaHeron()
+    {
+        double s = (ladoA + ladoB + ladoC) / 2;
+        return Math.Sqrt(s * (s - ladoA) * (s - ladoB) * (s - ladoC));
+    }
+
+    public double AreaSeno(double angulo, bool radianes) 
+    {
+        if (!radianes)
+        {
+            angulo = angulo * Math.PI / 180;
+        }
+        return (ladoA * ladoB * Math.Sin(angulo)) / 2;
+    }
+
+    public double AreaCircunscrito()
+    {
+        double s = (ladoA + ladoB + ladoC) / 2;
+        double r = (ladoA * ladoB * ladoC) / (4 * Math.Sqrt(s * (s - ladoA) * (s - ladoB) * (s - ladoC)));
+        return (Math.PI * r * r) / 4;
+    }
+}
---
+++ Unit 3 area de un triangulo/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    
+    static void Main(string[] args)
+    {
+       
+        Triangulo t = new Triangulo(10, 8, 6, 6, 8);
+
+        Console.WriteLine("El área del triángulo usando la fórmula base por altura es: " + t.AreaBaseAltura());
+        Console.WriteLine("El área del triángulo usando la fórmula de Herón es: " + t.AreaHeron());
+        Console.WriteLine("El área del triángulo usando la fórmula del seno de un ángulo en radianes es: " + t.AreaSeno(Math.PI / 3, true));
+        Console.WriteLine("El área del triángulo usando la fórmula del seno de un ángulo en grados es: " + t.AreaSeno(60, false));
+        Console.WriteLine("El área del triángulo usando la fórmula del radio del círculo circunscrito es: " + t.AreaCircunscrito());
+    }
+}
